da2dc515271e9a560a1a4ea234703f9f99930fe0
Aleksander Kwiatkowski <bobikx@poczta.fm>
2012-11-28 22:48:04 +0100

    puller

--- pull.rb
+++ pull.rb
@@ -6,7 +6,7 @@ wd = WebcamDownloader::Downloader.new
 wd.load_all_definition_files
 flat_urls = wd.defs
 
-flat_urls = [flat_urls.first]
+#flat_urls = [flat_urls.first]
 
 url = "root@192.168.0.7:/opt/webcam_downloader/"
 destination = ''
